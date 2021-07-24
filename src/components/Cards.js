f43acdd9d420101b5e396d3d7d18203925daa0a6
shahid Afridi <shahidafridi.baymax@gmail.com>
2021-07-24 19:50:14 +0600

    final commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,22 +10,22 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              src="images/boat.jpg"
+              text="Explore the beauty of sada-pathor, Shylet, Banglagesh"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
-              text="Travel through the Islands of Bali in a Private Cruise"
+              src="images/sunset.JPG"
+              text="Enjoy the beauty of Kaptai lake, Rangamati, bangladesh"
               label="Luxury"
               path="/services"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              src="images/BD.jpg"
+              text="Tracking to the mountains of Bandarban, Chottogram, Bangladesh"
               label="Mystery"
               path="/services"
             />
